2fd1bcd643b6d3ad62bcdd697a04f7dc101d68bd
Igor <igor-526@yandex.ru>
2023-06-09 17:54:21 +0300

    ssl

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from vkwave.bots import SimpleLongPollBot, SimpleCallbackBot
 bot = SimpleCallbackBot(
     group_id=config.group_id,
     host="127.0.0.1",
-    path="/",
-    port=80,
+    path="",
+    port=3001,
     tokens=config.bot_token,
     confirmation_key="f05fe273",
     secret="zhrVWfxtL1jBCR2qUezC",
