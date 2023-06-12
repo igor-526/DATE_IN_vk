45a523170260540ca0732cfaf2f2d57b7a008064
Igor <igor-526@yandex.ru>
2023-06-12 16:20:41 +0300

    config

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ import config
 from vkwave.bots import SimpleLongPollBot, SimpleCallbackBot
 
 
-# bot = SimpleLongPollBot(tokens=config.bot_token, group_id=config.group_id)
+bot = SimpleLongPollBot(tokens=config.bot_token, group_id=config.group_id)
 
-bot = SimpleCallbackBot(
-    group_id=config.group_id,
-    host="127.0.0.1",
-    path="",
-    port=3001,
-    tokens=config.bot_token,
-    confirmation_key="c7d00990",
-    secret="zhrVWfxtL1jBCR2qUezC",
-)
+# bot = SimpleCallbackBot(
+#     group_id=config.group_id,
+#     host="127.0.0.1",
+#     path="",
+#     port=3001,
+#     tokens=config.bot_token,
+#     confirmation_key="c7d00990",
+#     secret="zhrVWfxtL1jBCR2qUezC",
+# )
